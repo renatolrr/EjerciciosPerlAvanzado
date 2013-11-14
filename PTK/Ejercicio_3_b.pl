4f28674579cb36a395de702109e1d960b4082a6c
renato <renato@debian>
2013-11-14 18:18:38 +0100

    Ejercicios Perl-TK

---
+++ PTK/Ejercicio_3_b.pl
@@ -0,0 +1,32 @@
+﻿#!/usr/local/bin/perl
+
+# Los ejercicios están en https://github.com/renatolrr/EjerciciosPerlAvanzado
+use Tk;
+
+my $ventana = new MainWindow;
+
+$ventana->Label(-text => "Nombre:")->pack();
+my $nombre = $ventana -> Entry() -> pack();
+$ventana->Label(-text => "Apellidos:")->pack();
+my $apellido = $ventana -> Entry() -> pack();
+$ventana->Label(-text => "Email:")->pack();
+my $email = $ventana -> Entry() -> pack();
+
+
+my $boton = $ventana -> Button(-text => "Graba", 
+                -command =>\&pinchar);
+$boton -> pack();
+
+MainLoop;
+
+# func. ejecutada al pulsar el botón
+sub pinchar {
+	my $nom = $nombre->get();
+	my $ape = $apellido->get();
+	my $ema = $email->get();
+	my $tmp = "Nombre: " . $nom . " Apellidos: ". $ape . " Email: " . $ema ;
+    $ventana -> messageBox(-type=>"ok", -message=>$tmp);
+    open (alumno,">>alumno.txt");
+    print alumno $nom . ";" . $ape . ";" . $ema . "\n"; 
+    close (alumno); 
+}
---
+++ PTK/holamundo.pl
@@ -0,0 +1,15 @@
+﻿#!/usr/bin/perl
+use strict;
+use warnings;
+use Tk;
+
+my $ventana = MainWindow->new();
+$ventana->title('Ejemplo de Hola Mundo');
+$ventana->geometry("300x200"); # damos el tamaño en anchura y altura
+$ventana->resizable( 0, 0 ); #  no dejaremos que la redimensionen
+
+# $ventana->minsize(qw(200 100));
+
+$ventana->Label(-text => " Hola Mundo ")->pack();
+
+MainLoop();
