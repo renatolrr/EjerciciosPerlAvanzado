416235bc1976bc2b8b12d1834b17eabfcf3af40a
renato <renato@debian>
2013-12-21 11:09:01 +0100

    proyecto

---
+++ PROYECTO/Necrorama/Necrolito/Fase_0.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+# Los ejercicios están en https://github.com/renatolrr/EjerciciosPerlAvanzado
+
+use strict;
+use warnings;
+
+use v5.10;
+use DBI;
+use SQL::Abstract;
+
+print "Fase 0\n";
+print "Tu nombre.\n";
+
+
+my $bd_file_name = shift || 'personaje.sqlite';
+
+
+my $sql = SQL::Abstract->new;
+
+
+print "Nombre\n";
+my $nombre=<STDIN>;
+	
+my %record = (
+		nombre      => $nombre,
+);
+
+my $table='personaje';
+
+insert();
+
+sub insert{
+	my $dbh = DBI->connect( "dbi:SQLite:dbname=$bd_file_name" ) 
+		|| die "No puedo conectarme con $bd_file_name: $!\n";
+
+	my($stmt, @bind) = $sql-> insert ($table,\%record);
+
+	my $sth = $dbh->prepare($stmt);
+	$sth->execute( @bind );
+
+	my $rc = $dbh->disconnect();
+
+}
+
---
+++ PROYECTO/Necrorama/Necrolito/Fase_I.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+# Los ejercicios están en https://github.com/renatolrr/EjerciciosPerlAvanzado
+
+
+use strict;
+use warnings;
+
+use v5.10;
+use DBI;
+use SQL::Abstract;
+
+print "Fase I\n";
+print "Vida previa.\n";
+print "Redacta el texto de un obituario, lápida o necrológica que se haga eco de la muerte de tu personaje
+en la Tierra.\n";
+
+my $bd_file_name = shift || 'personaje.sqlite';
+
+
+my $sql = SQL::Abstract->new;
+
+
+print "Id\n";
+my $id=<STDIN>;
+
+my %where= (
+	id => $id
+);
+
+print "Vida anterior\n";
+my $vida_anterior=<STDIN>;
+
+my %fieldvals = (
+	vida_anterior => $vida_anterior,
+);
+
+my $table='personaje';
+	
+update();
+
+sub update{
+	my $dbh = DBI->connect( "dbi:SQLite:dbname=$bd_file_name" ) 
+		|| die "No puedo conectarme con $bd_file_name: $!\n";
+	my($stmt, @bind) = $sql-> update ($table,\%fieldvals,\%where);
+
+	my $sth = $dbh->prepare($stmt);
+	$sth->execute( @bind );
+
+
+	my $rc = $dbh->disconnect();
+}
+
+
+
+
+
