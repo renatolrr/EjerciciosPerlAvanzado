97a38f7f42af2255f5a23887a1680875ff3ce1a7
renato <renato@debian>
2013-12-23 19:34:01 +0100

    proyecto

--- PROYECTO/Interlingua/menu.pl
+++ PROYECTO/Interlingua/menu.pl
@@ -1,11 +1,21 @@
 ﻿#!/usr/bin/perl
 # Los ejercicios están en https://github.com/renatolrr/EjerciciosPerlAvanzado
 
+use strict;
+use warnings;
+
+use v5.10;
+use DBI;
+use SQL::Abstract;
+
 use lib qw(lib);
 
 use Tk;
 use PA::Moose::Palabra;
 use utf8; #problema acentos
+
+my $bd_file_name = shift || 'interlingua.sqlite';
+my $sql = SQL::Abstract->new;
 
 my $palabra;
 my $text;
@@ -19,8 +29,8 @@ my $menu = $ventana->Menu(-type => 'menubar');
 $ventana->configure(-menu => $menu);
 
 my $mUno = $menu->cascade(-label => '~Archivo', -tearoff => 0);
-$mUno->command(-label => '~Abrir', -command => \&func1);
-$mUno->command(-label => '~Guardar', -command => \&func2);
+#$mUno->command(-label => '~Abrir', -command => \&func1);
+#$mUno->command(-label => '~Guardar', -command => \&func2);
 $mUno->command(-label => '~Salir', -command => \&func3);
 
 my $mDos = $menu->cascade(-label => '~Edición', -tearoff => 0);
@@ -28,14 +38,19 @@ $mDos->command(-label => '~Borrar texto', -command => \&func4);
 
 
 my $mTres= $menu->cascade(-label => '~Texto', -tearoff => 0);
-$mTres->command(-label => '~Enviar', -command => \&func5);
-$mTres->command(-label => '~Histórico enviados', -command => \&func6);
+$mTres->command(-label => '~Guardar', -command => \&func5);
+$mTres->command(-label => '~Histórico guardados', -command => \&func6);
 
 
 my $mAyuda = $menu->cascade(-label => '~Ayuda', -tearoff => 0);
 $mAyuda->command(-label => '~Acerca de...', -command => \&func7);
 
+$ventana->Label(-text => "Texto")->pack(); 
+my $ent_texto= $ventana -> Text(-width=>140, -height=>1) -> pack();
+
+$ventana->Label(-text => "Busqueda palabra")->pack(); 
 my $ent = $ventana -> Entry() -> pack();
+
 my $boton = $ventana ->Button(-text=>"resuelve", -command =>\&resuelve);
 $boton->pack();
 $ventana->Label(-text => "Diccionario")->pack(); 
@@ -45,7 +60,7 @@ my $sin = $ventana -> Text(-width=>40, -height=>10) -> pack();
 $ventana->Label(-text => "Tesauro")->pack(); 
 my $tes = $ventana -> Text(-width=>40, -height=>20) -> pack();
 $ventana->Label(-text => "Texto")->pack(); 
-my $text = $ventana -> Text(-width=>40, -height=>5) -> pack();
+my $text2 = $ventana -> Text(-width=>40, -height=>5) -> pack();
 MainLoop;
 
 sub func1{
@@ -72,14 +87,32 @@ sub func4{
 	"https://github.com/renatolrr/EjerciciosPerlAvanzado");
 }
 sub func5{
-	$ventana -> messageBox(-type=>"ok", -message=>'Renato Ramirez' . "\n" . 
-	'renatolrr@gmail.com' . "\n" .  
-	"https://github.com/renatolrr/EjerciciosPerlAvanzado");
+	my $fecha = time();
+	my $tmp = $ent_texto->get('0.0','end');
+	my %record = (
+        fecha         	=> $fecha,
+        texto      		=> $tmp,
+        estado       	=> 0
+  	);
+	my $dbh = DBI->connect( "dbi:SQLite:dbname=$bd_file_name" ) 
+		|| die "No puedo conectarme con $bd_file_name: $!\n";
+	my($stmt, @bind) = $sql-> insert ('twitter_interlingua',\%record);
+	my $sth = $dbh->prepare($stmt);
+	$sth->execute( @bind );
+	my $rc = $dbh->disconnect();
+
+	
 }
 sub func6{
-	$ventana -> messageBox(-type=>"ok", -message=>'Renato Ramirez' . "\n" . 
-	'renatolrr@gmail.com' . "\n" .  
-	"https://github.com/renatolrr/EjerciciosPerlAvanzado");
+	my $dbh = DBI->connect( "dbi:SQLite:dbname=$bd_file_name" ) 
+    || die "No puedo conectarme con $bd_file_name: $!\n";
+	my $ary = $dbh->selectall_arrayref('select texto from twitter_interlingua');
+	my $men;
+	for (@$ary){
+		$men = $men . join ("\t",@$_);
+		}
+	$ventana -> messageBox(-type=>"ok", -message=> $men );
+	
 }
 sub func7{
 	$ventana -> messageBox(-type=>"ok", -message=>'Renato Ramirez' . "\n" . 
@@ -88,8 +121,8 @@ sub func7{
 }
 
 sub resuelve{
-	$temp=$ent->get();
-	$palabra = new PA::Moose::Palabra ( palabra => $temp);
+	my $temp=$ent->get();
+	my $palabra = new PA::Moose::Palabra ( palabra => $temp);
 	#borra
 	$dic->delete('0.0',"end");
 	$sin->delete('0.0',"end");
