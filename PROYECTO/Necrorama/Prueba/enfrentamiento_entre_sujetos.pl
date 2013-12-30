1c6957f985ab2ddb16b001416308e3fc8718f1fa
renato <renato@debian>
2013-12-30 09:20:28 +0100

    proyecto

---
+++ PROYECTO/Necrorama/Prueba/enfrentamiento_entre_sujetos.pl
@@ -0,0 +1,50 @@
+﻿#!/usr/bin/perl
+# Los ejercicios están en https://github.com/renatolrr/EjerciciosPerlAvanzado
+
+use strict;
+use warnings;
+
+use v5.10;
+
+
+#Hay que tener en cuenta la reserva, los modificadores, trabajo en equipo y equipo.
+my $rasgo_jug_1="atletismo 3";
+my $rasgo_jug_2="escape 5";
+
+my $dado;
+my $diferencia;
+my $resultado;
+
+
+tirar_dado();
+my $dado_jug_1=$dado;
+tirar_dado();
+my $dado_jug_2=$dado;
+
+
+my $rasgo_1=chop($rasgo_jug_1);
+my $rasgo_2=chop($rasgo_jug_2);
+$diferencia=$dado_jug_1+$rasgo_jug_1-$dado_jug_2+$rasgo_jug_2;
+
+if ($diferencia==0){
+	$resultado="Empate";
+}elsif($diferencia>=1){
+	$resultado="Gana Jugador 1";
+}else{
+	$resultado="Gana Jugador 2";
+}
+ print $resultado."\n";
+
+sub tirar_dado{ 
+	my $dado_1=int(rand(10))+1;
+	my $dado_2=int(rand(10))+1;
+
+
+	if ($dado_1>$dado_2){
+		$dado=$dado_1;	
+	}else{
+		$dado=$dado_2;
+	}
+
+}
+
---
+++ PROYECTO/Necrorama/Prueba/prueba_de_estres.pl
@@ -0,0 +1,60 @@
+﻿#!/usr/bin/perl
+# Los ejercicios están en https://github.com/renatolrr/EjerciciosPerlAvanzado
+
+use strict;
+use warnings;
+
+use v5.10;
+
+#Hay que tener en cuenta la reserva, los modificadores, trabajo en equipo y equipo.
+my $rasgo_jug_1="atletismo 3";
+#Tabla de resistencia
+#Chupada		3
+#Muy fácil		5
+#Fácil			7
+#Normal			9
+#Compleja		11
+#Muy compleja	15
+#Complicadisima	19
+#Imposible		24
+my $resistencia=5;
+my $dado;
+my $diferencia;
+my $resultado;
+
+my $dado_1=int(rand(10))+1;
+my $dado_2=int(rand(10))+1;
+
+if ($dado_1>$dado_2){
+	$dado=$dado_1;	
+}else{
+	$dado=$dado_2;
+}
+
+
+my $rasgo=chop($rasgo_jug_1);
+
+if (($dado_1&&$dado_2)<=$rasgo){
+	#Crítico;
+	$dado=20;
+}elsif(($dado_1&&$dado_2)>$rasgo){
+	#Pifiado;
+	$dado=-10;
+}
+
+$diferencia=$dado+$rasgo-$resistencia;
+
+if ($diferencia<=0){
+	$resultado="Empate";
+}elsif($diferencia>=1 && $diferencia<=5){
+	$resultado="Lo supera por poco";
+}elsif($diferencia>=6 && $diferencia<=10){
+	$resultado="Victoria holgada";
+}elsif($diferencia>=11 && $diferencia<=15){
+	$resultado="Lección magistral";
+}elsif($diferencia>=16 ){
+	$resultado="Perfección total";
+}	
+ print $resultado."\n";
+
+
